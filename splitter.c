a1ef79610dad6f27732b677e4ed607e55eb34526
Nishant singh <201951106@iiitvadodara.ac.in>
2019-10-06 10:20:51 +0530

    splitter code added

---
+++ splitter.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+    char fn[250], ch, nfn[250];
+    long int size, n, k;
+    int i;
+    FILE *f, *ft;
+
+    printf("Enter the name of the file: ");
+    scanf("%s", fn);
+    printf("\nEnter the number of parts you want to split the file : ");
+    scanf("%ld", &n);
+    f=fopen(fn, "rb");
+    if (f==NULL)
+    {
+        printf("couldn't open file");
+        exit(0);
+    }
+
+    fseek(f, 0, 2);
+    size = ftell(f);
+    i = 1;
+    k = size/n;
+    rewind(f);
+    sprintf(nfn, "%s.%d", fn, i);
+    ft = fopen(nfn, "wb");
+    while(1)
+    {
+        ch = fgetc(f);
+        if (ch==EOF)
+            break;
+        fputc(ch, ft);
+        if (ftell(f)==(i*k))
+        {
+            i = i+1;
+            fclose(ft);
+            sprintf(nfn, "%s.%d", fn, i);
+            ft=fopen(nfn, "wb");
+        }
+    }
+}
